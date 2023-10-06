e8a6fbc42b7e1f9aa26c6188a84b0b82f2b784a2
Amit <amitsivansh2001@gmail.com>
2023-10-06 22:14:46 +0530

    HomePage showing error in adding files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
-// import { Card, FormField, Loader } from '../components'
+import { Card, Form, Loader } from '../components'
 // import {Card} from '../components'
 // import {FormField} from '../components'
 // import {Loader} from '../components'
